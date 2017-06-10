44e0bf5a0d86970c194a57877d5c8b5931389280
rysmarie <lillynte@gmail.com>
2017-06-10 23:27:54 +0900

    Apply fix to assignment.py

--- RyutaShimogauchi/assignment.py
+++ RyutaShimogauchi/assignment.py
@@ -57,7 +57,7 @@ def assignment5():
 def assignment6():
     a = np.random.randint(1, 10, 5)
     b = np.random.randint(1, 10, 5)
-    c = np.dot(a, b) / (np.sqrt(np.sum(a*a)*np.sum(b*b)))
+    c = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
     print('a={}\nb={}\ncos of a and b is {}.'.format(a, b, c))
 
 if __name__ == '__main__':
