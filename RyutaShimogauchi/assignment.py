8d2c1b6a3a24a968ddfda83eec4541cb0dfece08
rysmarie <lillynte@gmail.com>
2017-06-10 20:03:41 +0900

    Add assgnment2

--- RyutaShimogauchi/assignment.py
+++ RyutaShimogauchi/assignment.py
@@ -4,5 +4,10 @@ import numpy as np
 def assignment1():
     listFrom1to10 = list(range(1, 11))
     print('{}'.format(listFrom1to10))
+
+def assignment2():
+    evenFrom1to10 = list(range(2, 11, 2))
+    print('{}'.format(evenFrom1to10))
 if __name__ == '__main__':
     assignment1()
+    assignment2()
