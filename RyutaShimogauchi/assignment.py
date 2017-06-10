324a6ed7009ab2a783b44c23f7ee5851508524a6
rysmarie <lillynte@gmail.com>
2017-06-10 20:33:38 +0900

    Add assignment4

--- RyutaShimogauchi/assignment.py
+++ RyutaShimogauchi/assignment.py
@@ -17,7 +17,37 @@ def assignment3():
     print('{}'.format(intAlphDict))
 
 
+class Human():
+    counter = 0
+
+    def __init__(self, name, age, sex, height, weight):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.height = height
+        self.weight = weight
+        self.incrementCounter()
+
+    def incrementAge(self):
+        self.age += 1
+
+    def incrementCounter(self):
+        Human.counter += 1
+
+
+def assignment4():
+    Yamada = Human("山田", 23, "male", 170.3, 60.2)
+    print('counter value:{}'.format(Human.counter))
+    print('{0.name}:\n\tage:{0.age}\n\tsex:{0.sex}\n\theight:{0.height}\n\tweight:{0.weight}\n'.format(Yamada))
+    Takahashi = Human("高橋", 30, "female", 165.2, 46.2)
+    print('counter value:{}'.format(Human.counter))
+    print('{0.name}:\n\tage:{0.age}\n\tsex:{0.sex}\n\theight:{0.height}\n\tweight:{0.weight}\n'.format(Takahashi))
+    Yamada.incrementAge()
+    print("increment {}'s age.".format(Yamada.name))
+    print('{0.name}:\n\tage:{0.age}\n\tsex:{0.sex}\n\theight:{0.height}\n\tweight:{0.weight}\n'.format(Yamada))
+
 if __name__ == '__main__':
     assignment1()
     assignment2()
     assignment3()
+    assignment4()
